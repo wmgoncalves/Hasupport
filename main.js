50e19b54b445c766b566539493b50e76ed44d5c9
fabioo-junioor <fj_011@hotmail.com>
2022-10-11 20:11:47 -0300

    otimização de arquivos

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ function createWindow () {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 1200,
-    height: 800,
+    height: 700,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
