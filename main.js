0c354e22dfb7b242ab528f29cbfbe443dd77f0be
Eduardo <eduardosantossm@hotmail.com>
2022-10-13 23:56:56 -0300

    cadastro

--- main.js
+++ main.js
@@ -13,8 +13,8 @@ function createWindow () {
   })
 
   // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
-  mainWindow.loadfile('enf.png')
+  mainWindow.loadFile('./pages/cadastro.html')
+ 
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
