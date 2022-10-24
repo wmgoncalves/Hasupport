f4eabcd09c62213982f6e3cf700e6f9ba9ecf565
fabioo-junioor <fj_011@hotmail.com>
2022-10-23 23:16:48 -0300

    formatação retornos de busca

--- js/getDados.js
+++
@@ -1,9 +0,0 @@
-const btn = document.querySelector('#teste');
-
-function carrega(){
-    console.log(localStorage.getItem('pas'), localStorage.getItem('pad'))
-    console.log(localStorage.getItem('peso'), localStorage.getItem('altura'))
-   
-}
-
-btn.addEventListener("click", carrega);
