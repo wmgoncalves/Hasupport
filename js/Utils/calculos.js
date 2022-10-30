296084cb24d5611bf9be8f36b09835013106b867
Wingli <87144918+wmgoncalves@users.noreply.github.com>
2022-10-30 16:18:02 -0300

    Update.calculo

--- js/Utils/calculos.js
+++ js/Utils/calculos.js
@@ -131,78 +131,179 @@ function torno_brac() {
 
 }
 
-function calcularEstratificacaoParte2(){
-	/*
+function calcularEstratificacaoParte2() {
+
 	let pontos = 0;
 	let idade = localStorage.getItem('userCadIdade')
-	
-	if((localStorage.getItem('userCadSexo')) === 'Masculino'){
-		if((idade > 20)&&(idade <= 34)){
-			pontos = -9;
+	let fumante = document.querySelector('#etapa2-fumante')
+	let pas2 = document.querySelector('#etapa2-pa-sistol')
+	let tratada = document.querySelector('#etapa2-tratada')
+	let perc_risco = "..."
+	if ((localStorage.getItem('userCadSexo')) === 'Masculino') {
+		if ((idade > 20) && (idade <= 34)) { pontos += -9 }
+		else if ((idade > 34) && (idade <= 39)) { pontos += -4 }
+		else if ((idade > 40) && (idade <= 44)) { pontos += 0 }
+		else if ((idade > 45) && (idade <= 49)) { pontos += 3 }
+		else if ((idade > 50) && (idade <= 54)) { pontos += 6 }
+		else if ((idade > 55) && (idade <= 59)) { pontos += 8 }
+		else if ((idade > 60) && (idade <= 64)) { pontos += 10 }
+		else if ((idade > 65) && (idade <= 69)) { pontos += 11 }
+		else if ((idade > 70) && (idade < 74)) { pontos += 12; }
+		else if ((idade > 75) && (idade <= 79)) { pontos += 13 }
 
-		}else if((idade > 34)&&(idade <= 39)){
-			pontos = -4;
-
-		}else if((idade > 40)&&(idade <= 44)){
-			pontos = 0;
-
-		}else if((idade > 45)&&(idade <= 49)){
-			pontos = 3;
-
-		}else if((idade > 50)&&(idade <= 54)){
-			pontos = 6;
-
-		}else if((idade > 55)&&(idade <= 59)){
-			pontos = 8;
-
-		}else if((idade > 60)&&(idade <= 64)){
-			pontos = 10;
-
-		}else if((idade > 65)&&(idade <= 69)){
-			pontos = 11;
-
-		}else if((idade > 70)&&(idade < 74)){
-			pontos = 12;
-
-		}else if((idade > 75)&&(idade <= 79)){
-			pontos = 13;
-
+		if (colesterol < 160) { pontos += 0 }
+		else if (colesterol <= 199) {
+			if (idade <= 39) { pontos += 4 }
+			else if (idade <= 49) { pontos += 3 }
+			else if (idade <= 59) { pontos += 2 }
+			else if (idade <= 69) { pontos += 1 }
+			else if (idade <= 79) { pontos += 0 }
 		}
-	}else{
-		if((idade >= 20)&&(idade <= 34)){
-			pontos = -7;
-
-		}else if((idade > 34)&&(idade <= 39)){
-			pontos = -3;
-
-		}else if((idade > 40)&&(idade <= 44)){
-			pontos = 0;
-
-		}else if((idade > 45)&&(idade <= 49)){
-			pontos = 3;
-
-		}else if((idade > 50)&&(idade <= 54)){
-			pontos = 6;
-
-		}else if((idade > 55)&&(idade <= 59)){
-			pontos = 8;
-
-		}else if((idade > 60)&&(idade <= 64)){
-			pontos = 10;
-
-		}else if((idade > 65)&&(idade <= 69)){
-			pontos = 12;
-
-		}else if((idade > 70)&&(idade < 74)){
-			pontos = 14;
-
-		}else if((idade > 75)&&(idade <= 79)){
-			pontos = 16;
-			
+		else if (colesterol <= 239) {
+			if (idade <= 39) { pontos += 7 }
+			else if (idade <= 49) { pontos += 5 }
+			else if (idade <= 59) { pontos += 3 }
+			else if (idade <= 69) { pontos += 1 }
+			else if (idade <= 79) { pontos += 0 }
 		}
+		else if (colesterol <= 279) {
+			if (idade <= 39) { pontos += 9 }
+			else if (idade <= 49) { pontos += 6 }
+			else if (idade <= 59) { pontos += 4 }
+			else if (idade <= 69) { pontos += 2 }
+			else if (idade <= 79) { pontos += 1 }
+		}
+		else {
+			if (idade <= 39) { pontos += 11 }
+			else if (idade <= 49) { pontos += 8 }
+			else if (idade <= 59) { pontos += 5 }
+			else if (idade <= 69) { pontos += 3 }
+			else if (idade <= 79) { pontos += 1 }
+		}
+		if (fumante.checked == true) {
+			if (idade <= 39) { pontos += 8 }
+			else if (idade <= 49) { pontos += 5 }
+			else if (idade <= 59) { pontos += 3 }
+			else if (idade <= 69) { pontos += 1 }
+			else if (idade <= 79) { pontos += 1 }
+		}
+
+		if (pas2 < 120) { pontos += 0 }
+		else if (pas2 <= 129) {
+			if (tratada.checked == true) { pontos += 1 }
+			else { pontos += 0 }
+		}
+		else if (pas2 <= 139) {
+			if (tratada.checked == true) { pontos += 2 }
+			else { pontos += 1 }
+		}
+		else if (pas2 <= 159) {
+			if (tratada.checked == true) { pontos += 2 }
+			else { pontos += 1 }
+		}
+		else {
+			if (tratada.checked == true) { pontos += 3 }
+			else { pontos += 2 }
+		}
+		if (pontos < 0) { perc_risco = '<1' }
+		else if (pontos <= 4) { perc_risco = '1' }
+		else if (pontos <= 6) { perc_risco = '2' }
+		else if (pontos == 7) { perc_risco = '3' }
+		else if (pontos == 8) { perc_risco = '4' }
+		else if (pontos == 9) { perc_risco = '5' }
+		else if (pontos == 10) { perc_risco = '6' }
+		else if (pontos == 11) { perc_risco = '8' }
+		else if (pontos == 12) { perc_risco = '10' }
+		else if (pontos == 13) { perc_risco = '12' }
+		else if (pontos == 14) { perc_risco = '16' }
+		else if (pontos == 15) { perc_risco = '20' }
+		else if (pontos == 16) { perc_risco = '25' }
+		else if (pontos >= 17) { perc_risco = '>30' }
+
+	} else {
+		if ((idade >= 20) && (idade <= 34)) { pontos += -7 }
+		else if ((idade > 34) && (idade <= 39)) { pontos += -3 }
+		else if ((idade > 40) && (idade <= 44)) { pontos += 0 }
+		else if ((idade > 45) && (idade <= 49)) { pontos += 3 }
+		else if ((idade > 50) && (idade <= 54)) { pontos += 6 }
+		else if ((idade > 55) && (idade <= 59)) { pontos += 8 }
+		else if ((idade > 60) && (idade <= 64)) { pontos += 10 }
+		else if ((idade > 65) && (idade <= 69)) { pontos += 12 }
+		else if ((idade > 70) && (idade < 74)) { pontos += 14 }
+		else if ((idade > 75) && (idade <= 79)) { pontos += 16 }
+
+		if (colesterol < 160) { pontos += 0 }
+		else if (colesterol <= 199) {
+			if (idade <= 39) { pontos += 4 }
+			else if (idade <= 49) { pontos += 3 }
+			else if (idade <= 59) { pontos += 2 }
+			else if (idade <= 69) { pontos += 1 }
+			else if (idade <= 79) { pontos += 1 }
+		}
+		else if (colesterol <= 239) {
+			if (idade <= 39) { pontos += 8 }
+			else if (idade <= 49) { pontos += 6 }
+			else if (idade <= 59) { pontos += 4 }
+			else if (idade <= 69) { pontos += 2 }
+			else if (idade <= 79) { pontos += 1 }
+		}
+		else if (colesterol <= 279) {
+			if (idade <= 39) { pontos += 11 }
+			else if (idade <= 49) { pontos += 8 }
+			else if (idade <= 59) { pontos += 5 }
+			else if (idade <= 69) { pontos += 3 }
+			else if (idade <= 79) { pontos += 2 }
+		}
+		else {
+			if (idade <= 39) { pontos += 13 }
+			else if (idade <= 49) { pontos += 10 }
+			else if (idade <= 59) { pontos += 7 }
+			else if (idade <= 69) { pontos += 4 }
+			else if (idade <= 79) { pontos += 2 }
+		}
+		if (fumante.checked == true) {
+			if (idade <= 39) { pontos += 9 }
+			else if (idade <= 49) { pontos += 7 }
+			else if (idade <= 59) { pontos += 4 }
+			else if (idade <= 69) { pontos += 2 }
+			else if (idade <= 79) { pontos += 1 }
+		}
+
+		if (pas2 < 120) { pontos += 0 }
+		else if (pas2 <= 129) {
+			if (tratada.checked == true) { pontos += 3 }
+			else { pontos += 1 }
+		}
+		else if (pas2 <= 139) {
+			if (tratada.checked == true) { pontos += 4 }
+			else { pontos += 2 }
+		}
+		else if (pas2 <= 159) {
+			if (tratada.checked == true) { pontos += 5 }
+			else { pontos += 3 }
+		}
+		else {
+			if (tratada.checked == true) { pontos += 6 }
+			else { pontos += 4 }
+		}
+		if (pontos < 9) { perc_risco = '<1' }
+		else if (pontos <= 12) { perc_risco = '1' }
+		else if (pontos <= 14) { perc_risco = '2' }
+		else if (pontos == 15) { perc_risco = '3' }
+		else if (pontos == 16) { perc_risco = '4' }
+		else if (pontos == 17) { perc_risco = '5' }
+		else if (pontos == 18) { perc_risco = '6' }
+		else if (pontos == 19) { perc_risco = '8' }
+		else if (pontos == 20) { perc_risco = '11' }
+		else if (pontos == 21) { perc_risco = '14' }
+		else if (pontos == 22) { perc_risco = '17' }
+		else if (pontos == 23) { perc_risco = '22' }
+		else if (pontos == 24) { perc_risco = '27' }
+		else if (pontos >= 25) { perc_risco = '>30' }
 	}
 	console.log(pontos, idade);
-	*/
+
+
 }
 btnPasPad.addEventListener("click", calcular_pas_pad);
 btnCirAbd.addEventListener("click", classif_circ_abdominal);
