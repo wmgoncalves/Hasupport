3c22b8cc34dcb54d983a94e6a429d0511801ceb2
fabioo-junioor <fj_011@hotmail.com>
2022-10-30 21:24:54 -0300

    ajustes ndo novo db

--- backend/user_sus/lastCad.js
+++ backend/user_sus/lastCad.js
@@ -4,7 +4,7 @@ const btnLastCadast = document.querySelector("#btn-print-ult-adic");
 
 async function printLastCad(){
     let result = document.querySelector("#return-busca");
-    result.innerHTML = `<table border="1">
+    /*result.innerHTML = `<table border="1">
         <thead>
             <tr>
                 <th> Cartão do SUS </th>
@@ -14,21 +14,32 @@ async function printLastCad(){
                 <th> Sexo </th>
             </tr>
         </thead>
-        `;
+        `;*/
 
     let idUserSus = await lastUserSus();
     let rows = await loadUserSus(idUserSus[0].lastId);
 
     let data_nasc = rows[0].data_nasc.getDate() + '/' + (rows[0].data_nasc.getMonth()+1) + '/' + rows[0].data_nasc.getFullYear();
 
-    result.innerHTML += `<tbody>
-            <tr>
-                <td> ${rows[0].cart_sus} </td>
-                <td> ${rows[0].cpf} </td>
-                <td> ${rows[0].nome} </td>
-                <td> ${data_nasc} </td>
-                <td> ${rows[0].sexo} </td>
-            </tr>
+    result.innerHTML = `
+        <table border="1">
+            <thead>
+                <tr>
+                    <th> Cartão do SUS </th>
+                    <th> CPF </th>
+                    <th> Nome </th>
+                    <th> Data de Nasc. </th>
+                    <th> Sexo </th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td> ${rows[0].cart_sus} </td>
+                    <td> ${rows[0].cpf} </td>
+                    <td> ${rows[0].nome} </td>
+                    <td> ${data_nasc} </td>
+                    <td> ${rows[0].sexo} </td>
+                </tr>
             </tbody>
         </table>`;
 }
