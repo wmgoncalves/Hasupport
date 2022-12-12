80f24e03b0034a93c70a2929de0cbc05e0b0b770
Lucas Ziquinatti Piccini Nunes <72109329+Ziquinatti@users.noreply.github.com>
2022-12-11 23:05:44 -0300

    Consulta + Salvar Necessidades
    Consultas dos usuários do sus com alto risco e último cadastrado estão prontas.
    
    Além disso, habilitei as telas de Diagnóstico e criei um protótipo utilizando a função querySelectorAll. Coloquei na tela de Oxigenação, é possível ver o resultado no console.

--- js/saveStorageNhb/oxigenacao.js
+++ js/saveStorageNhb/oxigenacao.js
@@ -1,12 +1,38 @@
 const btnOxigenacaoSalvar = document.querySelector('#btn-oxigenacao-salvar');
 
+function save(){
+    let checks = document.querySelectorAll("div > input");
+
+    localStorage.setItem('qtd-oxi', checks.length);
+    for(let i=0; i<checks.length; i++){
+        if(checks[i].checked == true){
+            let name = `oxi-${i}`;
+            localStorage.setItem(name, checks[i].name);
+        }
+    }
+}
+
+function test(){
+    let tot = localStorage.getItem('qtd-oxi');
+    for(let i=0; i<tot; i++){
+        let name = `oxi-${i}`;
+        let check = localStorage.getItem(name);
+        console.log(check);
+    }
+}
+
 function insertOxigenacao(){
     let el = document.querySelector('#retorno-oxigenacao');
+    console.log("Salvando...");
+    save();
+    
+    console.log("Testando...");
+    test();
+    
     el.innerHTML = "Salvo com sucesso";
     setTimeout(function(){
 
     }, 2000);
-    
 }
 
 btnOxigenacaoSalvar.addEventListener("click", insertOxigenacao);
