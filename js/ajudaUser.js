cc0d12aa71756e5442c21c9a79aae537c312523d
fabioo-junioor <fj_011@hotmail.com>
2023-01-10 14:13:49 -0300

    verificação tamanho cpf/cartão sus

--- js/ajudaUser.js
+++ js/ajudaUser.js
@@ -3,7 +3,7 @@ const btnAjuda = document.querySelector('#btn-ajuda');
 function ajudaUser(){
     const el = document.querySelector('#retorno-ajuda-user');
     el.innerHTML = 'https://www.youtube.com/channel/UCSHtFhjmheShsGOyvWqdVjA';
-    el.style.fontSize = '1.2rem';
+    el.style.fontSize = '1rem';
     el.style.color = 'green';
     el.style.fontWeight = '500';
     setTimeout(function(){
