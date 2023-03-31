f4a1a8cebde27fac4b54e41e1b5f1389fae6639b
Eduardo <eduardosantossm@hotmail.com>
2023-03-30 21:12:08 -0300

    calculo imc

--- js/insertDadosStorageAvaClinic.js
+++ js/insertDadosStorageAvaClinic.js
@@ -368,31 +368,53 @@ function calculaImc(peso, altura) {
     if (result < 18.5) {
         localStorage.setItem('clas_imc', 'Abaixo do peso');
         el.innerHTML = 'Abaixo do peso'
-        el.innerHTML = 'Seu IMC é ' + result.toFixed(2);
 
     } else if ((result >= 18.5) && (result < 25)) {
         localStorage.setItem('clas_imc', 'Peso normal');
         el.innerHTML = 'Peso normal'
-        el.innerHTML = 'Seu IMC é ' + result.toFixed(2);
 
     } else if ((result >= 25) && (result < 30)) {
         localStorage.setItem('clas_imc', 'Excesso de peso');
         el.innerHTML = 'Excesso de peso'
-        el.innerHTML = 'Seu IMC é ' + result.toFixed(2);
 
     } else if ((result >= 30) && (result < 35)) {
         localStorage.setItem('clas_imc', 'Obesidade classe I');
         el.innerHTML = 'Obesidade classe I'
-        el.innerHTML = 'Seu IMC é ' + result.toFixed(2);
 
     } else if ((result >= 35) && (result < 40)) {
         localStorage.setItem('clas_imc', 'Obesidade classe II');
         el.innerHTML = 'Obesidade classe II'
-        el.innerHTML = 'Seu IMC é ' + result.toFixed(2);
 
     } else if (result >= 40) {
         localStorage.setItem('clas_imc', 'Obesidade classe III');
         el.innerHTML = 'Obesidade classe III'
+
+    }
+}
+function calculaImc2(peso, altura) {
+    let el = document.querySelector('#retorno-calculo-imc2');
+    let result = parseInt(peso) / (parseFloat(altura) * parseFloat(altura));
+    console.log(result);
+    
+
+    localStorage.setItem('imc', result);
+
+    if (result < 18.5) {
+        el.innerHTML = 'Seu IMC é ' + result.toFixed(2);
+
+    } else if ((result >= 18.5) && (result < 25)) {
+        el.innerHTML = 'Seu IMC é ' + result.toFixed(2);
+
+    } else if ((result >= 25) && (result < 30)) {
+        el.innerHTML = 'Seu IMC é ' + result.toFixed(2);
+
+    } else if ((result >= 30) && (result < 35)) {
+        el.innerHTML = 'Seu IMC é ' + result.toFixed(2);
+
+    } else if ((result >= 35) && (result < 40)) {
+        el.innerHTML = 'Seu IMC é ' + result.toFixed(2);
+
+    } else if (result >= 40) {
         el.innerHTML = 'Seu IMC é ' + result.toFixed(2);
 
     }
