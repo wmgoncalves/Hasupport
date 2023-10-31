9e13e288d03e1fc4b2d3828759152530362c3d69
Wingli <87144918+wmgoncalves@users.noreply.github.com>
2023-10-31 14:14:56 -0300

    mudança youtube

--- js/ajudaUser.js
+++ js/ajudaUser.js
@@ -2,7 +2,7 @@ const btnAjuda = document.querySelector('#btn-ajuda');
 
 function ajudaUser(){
     const el = document.querySelector('#retorno-ajuda-user');
-    el.innerHTML = 'https://www.youtube.com/channel/UCSHtFhjmheShsGOyvWqdVjA';
+    el.innerHTML = 'https://www.youtube.com/watch?v=rH_A1A4rBAM';
     el.style.fontSize = '1rem';
     el.style.color = 'green';
     el.style.fontWeight = '500';
