20e8bca25f7ed0b719eea0f1cccfa02ab441a5af
Wingli <87144918+wmgoncalves@users.noreply.github.com>
2023-11-12 16:13:40 -0300

    erros

--- js/ajudaUser.js
+++ js/ajudaUser.js
@@ -2,14 +2,14 @@ const btnAjuda = document.querySelector('#btn-ajuda');
 
 function ajudaUser(){
     const el = document.querySelector('#retorno-ajuda-user');
-    el.innerHTML = 'https://www.youtube.com/watch?v=rH_A1A4rBAM';
+    el.innerHTML = 'https://www.youtube.com/@HaAssistAssist/playlists';
     el.style.fontSize = '1rem';
     el.style.color = 'green';
     el.style.fontWeight = '500';
     setTimeout(function(){
         el.innerHTML = '';
         
-    }, 7000);
+    }, 7000);s
     
 }
 btnAjuda.addEventListener("click", ajudaUser);
